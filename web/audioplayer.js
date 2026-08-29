21e0d49912cbb580f80b606d18a361a615d88617
Nishi <nishi@nishi.boats>
2026-08-29 22:57:24 +0900

    separate things to audioplayer

---
+++ web/audioplayer.js
@@ -0,0 +1,50 @@
+class AudioPlayer {
+	constructor(audioContext, rate){
+		let sources = [];
+		let samples = 0;
+		const baseTime = audioContext.currentTime;
+
+		const frames = rate / 100;
+
+		this.paused = true;
+		this.onbuffer = ()=>{};
+
+		const read = ()=>{
+			const audioBuffer = audioContext.createBuffer(2, frames, rate);
+
+			if(!this.paused){
+				this.onbuffer(audioBuffer, frames);
+			}
+
+			const currentSource = audioContext.createBufferSource();
+			currentSource.buffer = audioBuffer;
+			currentSource.connect(audioContext.destination);
+
+			currentSource.onended = function() {
+				sources = sources.filter((x)=>x != currentSource);
+
+				for(let i = sources.length; i < 5; i++) read();
+			}
+
+			sources.push(currentSource);
+
+			currentSource.start(baseTime + samples / rate);
+
+			samples += frames;
+		}
+
+		read();
+		read();
+		read();
+		read();
+		read();
+	}
+
+	resume(){
+		this.paused = false;
+	}
+
+	pause(){
+		this.paused = true;
+	}
+};
