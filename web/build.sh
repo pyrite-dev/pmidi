5de458134306cd446d1924668741d9d4c952775d
Nishi <nishi@nishi.boats>
2026-08-29 13:31:01 +0900

    forgot

--- web/build.sh
+++ web/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ -d ../buildwasm ]; then
-	( cd ../buildwasm && make -j4 )
+	( cd ../buildwasm && make -j4 && cp lib/turbosynthwasm.js ../web/ )
 elif [ -d ../build ]; then
-	( cd ../build && make -j4 )
+	( cd ../build && make -j4 && cp lib/turbosynthwasm.js ../web/ )
 fi
 ( cd ../patches && zip -rv ../web/ct2mgm.zip ct2mgm*)
