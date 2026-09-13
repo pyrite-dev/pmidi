7fd63a017c47d268663838b08da0ac248452a9d8
Nishi <nishi@nishi.boats>
2026-09-13 12:29:22 +0900

    linux thread

--- src/thread.c
+++ src/thread.c
@@ -60,12 +60,71 @@ void Mutex_Unlock(Mutex* self) {
 }
 
 void Mutex_Destroy(Mutex* self) {
+	CloseHandle(self->event);
 	free(self);
 }
 #else
+#include <pthread.h>
+
 struct Thread {
+	pthread_t thread;
+	void (*callback)(void* user);
+	void* user;
 };
 
 struct Mutex {
+	pthread_mutex_t mutex;
 };
+
+static void* thread_func(void* arg) {
+	Thread* self = arg;
+
+	self->callback(self->user);
+
+	pthread_exit(NULL);
+
+	return NULL; /* is this needed? */
+}
+
+Thread* Thread_New(void (*callback)(void* user), void* user) {
+	Thread* self = malloc(sizeof(*self));
+
+	self->callback = callback;
+	self->user     = user;
+
+	pthread_create(&self->thread, NULL, thread_func, self);
+
+	return self;
+}
+
+void Thread_Wait(Thread* self) {
+	void* val;
+
+	pthread_join(self->thread, &val);
+}
+
+void Thread_Destroy(Thread* self) {
+	free(self);
+}
+
+Mutex* Mutex_New(void) {
+	Mutex* self = malloc(sizeof(*self));
+
+	pthread_mutex_init(&self->mutex, NULL);
+
+	return self;
+}
+
+void Mutex_Lock(Mutex* self) {
+	pthread_mutex_lock(&self->mutex);
+}
+
+void Mutex_Unlock(Mutex* self) {
+	pthread_mutex_unlock(&self->mutex);
+}
+
+void Mutex_Destroy(Mutex* self) {
+	pthread_mutex_destroy(&self->mutex);
+	free(self);
+}
 #endif
